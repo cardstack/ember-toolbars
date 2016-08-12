73877965a07b946bff7da1caec2d3fa6aca29627
Edward Faulkner <ef@alum.mit.edu>
2016-08-12 10:58:50 -0400

    size params for squishable-container

--- tests/dummy/app/components/squishable-container.js
+++ tests/dummy/app/components/squishable-container.js
@@ -1,8 +1,25 @@
 import Ember from 'ember';
 import { requestAnimationFrame } from 'cardstack-toolbars';
 
+const allowedUnits = ['em', 'vw', 'px', 'rem', 'ex'];
+
 export default Ember.Component.extend({
+  width: 100,
+  unit: 'vw',
   classNames: ['squishable-container'],
+
+  innerStyle: Ember.computed('width', 'unit', function() {
+    let width = this.get('width');
+    let unit = this.get('unit');
+    if (typeof width !== 'number') {
+      throw new Error(`squishable-container: width must be a number, not ${width}`);
+    }
+    if (allowedUnits.indexOf(unit) === -1) {
+      throw new Error(`squishable-container: unit must be one of [${allowedUnits.join(', ')}], not ${unit}`);
+    }
+    return Ember.String.htmlSafe(`width: ${width}${unit}`);
+  }),
+
   didInsertElement() {
     requestAnimationFrame(() => this.updateScale());
   },
