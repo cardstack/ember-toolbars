674dfdc0678574f6f5056f55f48c91398e3fe3f7
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 11:00:28 -0400

    remove accidental console.log

--- addon/transitions/adapt-margin.js
+++ addon/transitions/adapt-margin.js
@@ -7,7 +7,6 @@ export default function(dimension, opts) {
       let { measure } = propertiesFor(dimension);
       let size = parseInt(this.newElement.css(measure), 10);
       opts.adjust.forEach(({element, property}) => {
-        console.log(`adjusting ${property} to ${size}`, element[0]);
         animate(element, {[property]: `${size}px`}, { duration: 0, queue: false });
       });
     }
