94a0e0ec2aedd2c63e1a03646863c1d9dada7be5
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 09:01:53 -0400

    work in ember-cli < 2.7

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 module.exports = {
   name: 'ember-toolbars',
   included: function(app){
-    app.import('vendor/ember-toolbars.css');
+    if (this.import) { // support for ember-cli >= 2.7
+      this.import('vendor/ember-toolbars.css');
+    } else if (app.import) { // support ember-cli < 2.7
+      app.import('vendor/ember-toolbars.css');
+    }
   }
 };
