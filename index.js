e4a452dfb315840081bb78ef1ac3ad62ed236aea
Edward Faulkner <ef@alum.mit.edu>
2017-03-17 09:27:07 -0400

    ugh, so much import compat

--- index.js
+++ index.js
@@ -3,11 +3,33 @@
 
 module.exports = {
   name: 'ember-toolbars',
-  included: function(app){
-    if (this.import) { // support for ember-cli >= 2.7
-      this.import('vendor/ember-toolbars.css');
-    } else if (app.import) { // support ember-cli < 2.7
-      app.import('vendor/ember-toolbars.css');
+
+  init: function() {
+    if (this._super.init) {
+      this._super.init.apply(this, arguments);
     }
+    // Shim this.import for support in older versions of ember-cli
+    if (!this.import) {
+      this._findHost = function findHostShim() {
+        var current = this;
+        var app;
+
+        // Keep iterating upward until we don't have a grandparent.
+        // Has to do this grandparent check because at some point we hit the project.
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+
+        return app;
+      };
+      this.import = function importShim(asset, options) {
+        var app = this._findHost();
+        app.import(asset, options);
+      };
+    }
+  },
+
+  included: function(){
+    this.import('vendor/ember-toolbars.css');
   }
 };
